bf7eae663d37a467d58e11af4da25730921e5d9b
Lukasz Kula <lkula@future-processing.com>
2018-10-19 14:16:41 +0200

    Second Commit

---
+++ src/main/java/com/ice/cidemo/CidemoApplication.java
@@ -0,0 +1,12 @@
+package com.ice.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidemoApplication.class, args);
+	}
+}
