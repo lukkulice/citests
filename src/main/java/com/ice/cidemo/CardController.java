0367512d028caf31efcd65bde579a2ab8e39f2c6
Lukasz Kula <lkula@future-processing.com>
2018-10-19 15:18:30 +0200

    Added test controller

---
+++ src/main/java/com/ice/cidemo/CardController.java
@@ -0,0 +1,14 @@
+package com.ice.cidemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardController {
+
+  @GetMapping("/testing")
+  public String testController(){
+    return "This is Spring Rest Controller test";
+  }
+
+}
