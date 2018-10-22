a39aec989a6fb617ece87e51d30094f7c78ceb43
Lukasz Kula <lkula@future-processing.com>
2018-10-22 20:27:12 +0200

    some commit

--- src/main/java/com/ice/cidemo/CardController.java
+++ src/main/java/com/ice/cidemo/CardController.java
@@ -8,7 +8,7 @@ public class CardController {
 
   @GetMapping("/testing")
   public String testController(){
-    return "This is Spring Rest Controller test12356791011121";
+    return "This is Spring Rest Controller ";
   }
 
 }
