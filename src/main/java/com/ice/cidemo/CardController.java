07345993fcbe34b975e5854d32473fbeba75a6af
Lukasz Kula <lkula@future-processing.com>
2018-10-22 14:18:44 +0200

    cost tam

--- src/main/java/com/ice/cidemo/CardController.java
+++ src/main/java/com/ice/cidemo/CardController.java
@@ -8,7 +8,7 @@ public class CardController {
 
   @GetMapping("/testing")
   public String testController(){
-    return "This is Spring Rest Controller test1234";
+    return "This is Spring Rest Controller test1235";
   }
 
 }
